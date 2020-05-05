435ab34133c9a47b8b0bd963f261947510dd3e0c
sujanan <sujananbhathiya@gmail.com>
2020-05-05 20:49:19 +0530

    Add necessary comments

--- pool.c
+++ pool.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
 
@@ -8,6 +7,8 @@
 
 #define ARRAYLEN(a) (sizeof(a)/sizeof(a[0]))
 
+/// taskQueue implementation
+/// ========================
 #define taskQueueInit(q) ((q)->head = (q)->tail = (q)->n = 0)
 #define taskQueueFree(q) taskQueueInit(q)
 #define taskQueueIsEmpty(q) ((q)->n == 0)
@@ -25,6 +26,8 @@ struct task taskQueueDequeue(struct taskQueue *q) {
     return task; 
 }
 
+/// Semaphore manipulation
+/// ====================== 
 #define UP(s) (sem_post(s))
 #define DOWN(s) (sem_wait(s))
 #define FILLCOUNT_UP(p) UP(&p->fcount)
@@ -34,6 +37,18 @@ struct task taskQueueDequeue(struct taskQueue *q) {
 #define LOCK(p) DOWN(&p->lock)
 #define UNLOCK(p) UP(&p->lock)
 
+/// worker
+/// ======
+/// 1. Decrement the fill count.   
+/// 2. Enter the critical section.
+/// 3. Check whether the task queue is empty.
+///    If it is empty that means the pool is shutdown
+///    and this is only a wake up call. So, worker should
+///    stop working.
+/// 4. Get a task from the queue.
+/// 5. Leave the critical section.   
+/// 6. Increment the empty count.
+///
 static void *worker(void *arg) {
     struct pool *p = (struct pool *)arg;
 
@@ -50,6 +65,10 @@ static void *worker(void *arg) {
     UNLOCK(p);
 }
 
+/// poolInit
+/// ========
+/// Init queue and all the semaphores.
+///
 void poolInit(struct pool *p) {
     taskQueueInit(&p->q);
     sem_init(&p->fcount, 0, 0);
@@ -57,6 +76,10 @@ void poolInit(struct pool *p) {
     sem_init(&p->lock, 0, 1);
 }
 
+/// poolFree
+/// ========
+/// Destroy queue and all the semaphores.
+///
 void poolFree(struct pool *p) {
     sem_destroy(&p->fcount);
     sem_destroy(&p->ecount);
@@ -64,21 +87,54 @@ void poolFree(struct pool *p) {
     taskQueueFree(&p->q);
 }
 
+/// poolCreateWorkers
+/// =================
+/// Create all the workers in the pool.
+/// Workers will start working immediately.
+///
 void poolCreateWorkers(struct pool *p) {
     for (int i = 0; i < ARRAYLEN(p->workers); i++)
         pthread_create(&p->workers[i], NULL, worker, (void *)p);
 }
 
+/// poolShutdown
+/// ============
+/// Let workers know we have to shutdown. 
+/// This is not a immediate shutdown. Before the exit,
+/// Workers will finish all the work they are currently doing.
+/// Here poolShutdown will trigger extra ARRAYLEN(p->workers)
+/// (number of threads) increments to fill count to wake up the
+/// sleeping workers.
+///
 void poolShutdown(struct pool *p) {
     for (int i = 0; i < ARRAYLEN(p->workers); i++)
         FILLCOUNT_UP(p);
 }
 
+/// poolDestroyWorkers
+/// ==================
+/// Wait until all workers have finished their tasks
+/// to join them. Wokers will stop working when the pool
+/// is shutdown.
+///
 void poolDestroyWorkers(struct pool *p) {
     for (int i = 0; i < ARRAYLEN(p->workers); i++)
         pthread_join(p->workers[i], NULL);
 }
 
+/// poolAddTask
+/// ===========
+/// Add a new task to the queue.
+/// 1. Decrement the empty count.
+/// 2. Enter the critical section.
+/// 3. Add the new task.
+/// 4. Leave the critical section.
+/// 5. increment the fill count. 
+///
+/// If the empty count is 0 that means the task queue is full.
+/// poolAddTask will wait at this point until any of the workers
+/// pick a task from the queue.
+///
 void poolAddTask(struct pool *p, struct task t) {
     EMPTYCOUNT_DOWN(p);
     LOCK(p);
--- pool.h
+++ pool.h
@@ -6,13 +6,24 @@
 
 typedef void (*taskFn)(void *);
 
+/// task
+/// ====
+/// @fn: task function.
+/// @arg: Arguments of the @fn.
 struct task {
     taskFn fn;
     void *arg; 
 };
 
+/// Max number of tasks, task queue can hold.
 #define TASK_QUEUE_SIZE 16
 
+/// taskQueue
+/// =========
+/// @tasks: Tasks array.
+/// @head: Queue head position.
+/// @tail: Queue tail position.
+/// @n: Number of tasks in queue.
 struct taskQueue {
     struct task tasks[TASK_QUEUE_SIZE];
     int head;
@@ -20,8 +31,16 @@ struct taskQueue {
     int n;
 };
 
+/// Number of workers (threads) in the thread pool.
 #define POOL_WORKER_SIZE 4
 
+/// pool
+/// ====
+/// @fcount: Fill count semaphore.
+/// @ecount: Empty count semaphore.
+/// @lock: Locking semaphore for mutual execution.
+/// @q: Task queue.
+/// @workers: Working threads of the pool.
 struct pool {
     sem_t fcount;
     sem_t ecount;
@@ -30,6 +49,9 @@ struct pool {
     pthread_t workers[POOL_WORKER_SIZE];
 };
 
+/// ========
+/// Main API
+/// ========
 void poolInit(struct pool *p);
 void poolFree(struct pool *p);
 void poolCreateWorkers(struct pool *p);
